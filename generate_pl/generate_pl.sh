8c822daf3a1f9eb67b662267db5e01da424cf7f8
Yaroslav Krytsun <slavko7@gmail.com>
2024-11-30 11:27:21 +0200

    Folders

--- generate_pl/generate_pl.sh
+++ generate_pl/generate_pl.sh
@@ -3,5 +3,7 @@ set -e
 
 filename="${1:-pl}"
 
-find "$(pwd)" -type f | grep .mp3 > "$filename.m3u" && find "$(pwd)" -type f | grep .ogg >> "$filename.m3u" \
+echo "#EXTM3U" > $filename.m3u
+
+find "$(pwd)" -type f | grep .mp3 >> "$filename.m3u" && find "$(pwd)" -type f | grep .ogg >> "$filename.m3u" \
 && find "$(pwd)" -type f | grep .m4a >> "$filename.m3u"
